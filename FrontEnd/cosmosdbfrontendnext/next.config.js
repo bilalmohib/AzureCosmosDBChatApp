9583d224fb899bcaf3d9360a27cfa9aec6111259
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2023-09-17 17:21:41 +0500

    Working on ui of chat app

--- FrontEnd/cosmosdbfrontendnext/next.config.js
+++ FrontEnd/cosmosdbfrontendnext/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "plus.unsplash.com", "t4.ftcdn.net"],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- FrontEnd/cosmosdbfrontendnext/src/app/page.tsx
+++ FrontEnd/cosmosdbfrontendnext/src/app/page.tsx
@@ -1,132 +1,93 @@
 "use client";
-import { useEffect, useState } from "react";
-import { CosmosClient } from "@azure/cosmos";
 import { NextPage } from "next";
-
-// Define your configuration values here
-const endpoint = "https://mlsa2023.documents.azure.com:443/";
-const key =
-  "gSSJFs4KU9o90TjKBFdtGab7VGHR9FblA25bdLS8NxLt7e3kZRIprDXcWLTlhdqE5SassUchZCgAACDb6QgH3w==";
-const databaseName = "checkdb";
-const containerName = "checkcontainer";
-
-interface Product {
-  id: string;
-  categoryId: string;
-  categoryName: string;
-  sku: string;
-  name: string;
-  description: string;
-  price: number;
-  tags: { _id: string; name: string }[];
-}
+import { useState } from "react";
+import { BsFilter } from "react-icons/bs";
+import { BsPencilSquare } from "react-icons/bs";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  // const [products, setProducts] = useState<Product[]>([]);
-
-  // // To post the data these states are used
-  // const [categoryName, setCategoryName] = useState("");
-  // const [sku, setSku] = useState("");
-  // const [name, setName] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [price, setPrice] = useState(0);
-  // const [tags, setTags] = useState("");
-  // const [tagArray, setTagArray] = useState([]);
-
-  // const createDatabase = async () => {
-  //   const cosmosClient = new CosmosClient({ endpoint, key });
-
-  //   // Create database if it doesn't exist
-  //   const { database } = await cosmosClient.databases.createIfNotExists({
-  //     id: databaseName,
-  //   });
-  //   console.log(`${database.id} database ready`);
-
-  //   // Create container if it doesn't exist
-  //   const { container } = await database.containers.createIfNotExists({
-  //     id: containerName,
-  //   });
-  //   console.log(`${container.id} container ready`);
-  // };
-
-  // const cosmosClient = new CosmosClient({ endpoint, key });
-  // const database = cosmosClient.database(databaseName);
-  // const container = database.container(containerName);
-
-  // const fetchProducts = async () => {
-  //   // Query all items in the container
-  //   const { resources }: any = await container.items.readAll().fetchAll();
-
-  //   // Reverse the order of the items
-  //   const new_data = resources.reverse();
-
-  //   // Update the products state with the fetched data
-  //   setProducts(new_data);
-  // };
-
-  // const populateData = async () => {
-  //   // Split the tags string into an array
-  //   const tagArray = tags.split(",").map((tag) => {
-  //     return { _id: Math.random().toString(), name: tag };
-  //   });
-
-  //   // Data items
-  //   const item = {
-  //     id: Math.random().toString(),
-  //     categoryId: Math.random().toString(),
-  //     categoryName: categoryName,
-  //     sku: sku,
-  //     name: name,
-  //     description: description,
-  //     price: price,
-  //     tags: tagArray,
-  //   };
-
-  //   // Create all items
-  //   const { resources }: any = await container.items.create(item);
-  //   console.log(`'${resources}' inserted`);
-
-  //   // Update the products state with the fetched data
-  //   setProducts(
-  //     products.concat({
-  //       id: item.id,
-  //       categoryId: item.categoryId,
-  //       categoryName: item.categoryName,
-  //       sku: item.sku,
-  //       name: item.name,
-  //       description: item.description,
-  //       price: item.price,
-  //       tags: item.tags,
-  //     })
-  //   );
-
-  //   // Reset the form
-  //   setCategoryName("");
-  //   setSku("");
-  //   setName("");
-  //   setDescription("");
-  //   setPrice(0);
-  //   setTags("");
-  //   setTagArray([]);
-
-  //   alert("Data inserted successfully");
-  // };
-
-  // useEffect(() => {
-  //   createDatabase();
-  //   fetchProducts();
-  // }, []);
+  const [currentSelectedChat, setCurrentSelectedChat] = useState<number>(0);
 
   return (
     <main>
       <div className="rounded-none min-h-screen">
         <div className="bg-primary-50 w-full h-14"></div>
-        <div className="border-2 border-red-500 border-dotted w-full h-[calc(100vh-56px)] flex">
-          <div className="w-72 bg-[#f0f0f0] border-green-400 border-dotted border-4 h-full">
-            Left Side
+        <div className="w-full h-[calc(100vh-56px)] flex">
+          <div className="w-72 bg-[#f0f0f0] h-full">
+            <div className="border-b-[1.5px] border-gray-300 border-solid h-16 flex flex-col justify-center items-center">
+              <div className="flex justify-between w-full pl-4 pr-3">
+                <h3 className="text-2xl font-semibold">Chat</h3>
+                <div className="flex justify-end items-end ml-4">
+                  <button className="rounded-full w-8 h-8 flex justify-center items-center hover:scale-110 transform transition-all duration-200 ease-in-out text-[#424242] hover:text-[#5b5fc7]">
+                    <BsFilter size={25} color="inherit" />
+                  </button>
+                  <button className="rounded-full w-8 h-8 flex justify-center items-center hover:scale-110 transform transition-all duration-200 ease-in-out text-[#424242] hover:text-[#5b5fc7]">
+                    <BsPencilSquare size={20} color="inherit" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            {[0, 1, 2, 3, 4, 5].map((item, index) => {
+              return (
+                <div
+                  key={item}
+                  className={`h-12 flex flex-col justify-center items-center px-1`}
+                >
+                  <div
+                    className={`flex justify-between w-full pl-4 pr-3 h-full cursor-pointer rounded-md ${
+                      currentSelectedChat === index
+                        ? "hover:bg-white"
+                        : "hover:bg-[#fafafa]"
+                    } 
+                  ${currentSelectedChat === index ? "bg-white" : ""}
+                  `}
+                    onClick={() => setCurrentSelectedChat(index)}
+                  >
+                    <div className="flex flex-col justify-center items-center">
+                      <div className="flex">
+                        <Image
+                          src="https://t4.ftcdn.net/jpg/02/90/27/39/360_F_290273933_ukYZjDv8nqgpOBcBUo5CQyFcxAzYlZRW.jpg"
+                          alt="Picture of the author"
+                          width={30}
+                          height={30}
+                          className="rounded-full w-8 h-8 block border-2 border-gray-400 border-solid"
+                        />
+                        <h3 className="font-normal mt-[2.5px] ml-3 text-[#424242]">
+                          John Doe
+                        </h3>
+                      </div>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                      <h3 className="m-0 text-sm -mt-[2px] text-gray-500 font-normal">
+                        2:30 PM
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-          <div className="shadow-2xl w-[calc(100vw-288px)] bg-[#f5f5f5] border-green-400 border-solid border-4 h-full">
-            Right Side
+          <div className="shadow-2xl w-[calc(100vw-288px)] bg-[#f5f5f5] h-full">
+            <div className="border-b-[1.5px] border-gray-200 border-solid h-16 flex justify-between items-center px-4">
+              <h3 className="text-2xl">
+                {currentSelectedChat === 0
+                  ? "John Doe"
+                  : currentSelectedChat === 1
+                  ? "Jane Doe"
+                  : currentSelectedChat === 2
+                  ? "John Doe"
+                  : currentSelectedChat === 3
+                  ? "Jane Doe"
+                  : currentSelectedChat === 4
+                  ? "John Doe"
+                  : currentSelectedChat === 5
+                  ? "Jane Doe"
+                  : ""}
+              </h3>
+              <h3 className="text-2xl">
+                Right Side
+              </h3>
+            </div>
           </div>
         </div>
       </div>
